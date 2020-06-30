46bb18fe0ce528c9fd4d56c0f3b46e2c22c03ac6
sjh <742673570@qq.com>
2020-06-30 11:43:48 +0800

    增加一条注释

--- src/main/java/com/springboot/jpa/dao/HusbandWifeRespository.java
+++ src/main/java/com/springboot/jpa/dao/HusbandWifeRespository.java
@@ -10,4 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface HusbandWifeRespository extends JpaRepository<Wife,Integer> {
     Wife findByHusband_Hid(Integer hid);
+    //增加一条注释
 }
